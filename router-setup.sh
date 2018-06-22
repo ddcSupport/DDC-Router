a80e04baf711ba17f96fa10c47803b363b9db732
ddcSupport <ddcSupport@users.noreply.github.com>
2018-06-22 12:32:41 -0500

    Update router-setup.sh

--- router-setup.sh
+++ router-setup.sh
@@ -1,2 +1,5 @@
-# Setup
+# Software
+apt-get -y install bridge-utils hostapd build-essential wget curl gcc make wget tzdata git libreadline-dev libncurses-dev libssl-dev zlib1g-dev
+
+# Networking
 echo "net.ipv4.ip_forward = 1" > /etc/sysctl.d/10-ipv4_forwarding.conf
